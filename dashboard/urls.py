347ff58299785cf46de2334b32ede401f47d71f7
Thomas <74390441+ThomRib@users.noreply.github.com>
2026-01-07 01:18:04 -0800

    crud + ajax jquery
    crud + jquery ajax

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    # Authentication URLs
+    # path('login/', views.indexlogin, name='login'),
+    # path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # Dashboard
     path('', views.index, name='index'),
     path('data_karyawan/', views.data_karyawan, name='data_karyawan'),
+
+    # Ajax
+    path('get_karyawan/', views.get_karyawan, name='get_karyawan'),
+    path('delete_karyawan/', views.delete_karyawan, name='delete_karyawan'),
     path('input_karyawan/', views.input_karyawan, name='input_karyawan'),
-    path('logout/', views.logout_view, name='logout'),
+    path('update_karyawan/', views.update_karyawan, name='update_karyawan'),
+
 ]
